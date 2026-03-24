5cb369668982e33b4dbfa673fcecbf14baf2edd9
Melika Shooryabi <138528779+Melikash98@users.noreply.github.com>
2026-03-24 19:52:49 +0330

    Initial release of MorphNavBar

---
+++ morphnavbar/src/main/java/com/melikash98/morphnavbar/OnTabSelectedListener.java
@@ -0,0 +1,10 @@
+package com.melikash98.morphnavbar;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Callback for tab selection changes.
+ */
+public interface OnTabSelectedListener {
+    void onTabSelected(int index, @NonNull TabItem item);
+}
