64cd57963a65c5395a6a7437c20f9d9a8e4a3312
Melika Shooryabi <138528779+Melikash98@users.noreply.github.com>
2026-03-24 18:06:01 +0330

    Initial release of MorphNavBar

--- morphnavbar/src/main/java/com/melikash98/morphnavbar/TabButton.java
+++ morphnavbar/src/main/java/com/melikash98/morphnavbar/TabButton.java
@@ -1,16 +1,27 @@
 package com.melikash98.morphnavbar;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+
 public class TabButton  extends FrameLayout {
     public final ImageView iconView;
 
     public TabButton(Context context) {
-        super(context);
+        this(context, null);
+    }
+
+    public TabButton(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TabButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         setClickable(true);
         setFocusable(true);
         setBackground(null);
