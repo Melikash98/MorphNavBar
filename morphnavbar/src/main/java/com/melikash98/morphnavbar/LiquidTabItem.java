acc962dc066a170b66ca2b733b238195b698e1f6
Melika Shooryabi <138528779+Melikash98@users.noreply.github.com>
2026-03-25 19:19:06 +0330

    Initial release of MorphNavBar

--- morphnavbar/src/main/java/com/melikash98/morphnavbar/LiquidTabItem.java
+++ morphnavbar/src/main/java/com/melikash98/morphnavbar/LiquidTabItem.java
@@ -21,37 +21,50 @@ public final class LiquidTabItem {
     @Nullable
     private final CharSequence contentDescription;
 
+    @Nullable
+    private final CharSequence label;
+
     private LiquidTabItem(@NonNull Drawable icon,
                           @Nullable Drawable selectedIcon,
-                          @Nullable CharSequence contentDescription) {
+                          @Nullable CharSequence contentDescription,
+                          @Nullable CharSequence label) {
         this.icon = icon;
         this.selectedIcon = selectedIcon;
         this.contentDescription = contentDescription;
+        this.label = label;
     }
 
     @NonNull
     public static LiquidTabItem of(@NonNull Drawable icon, @Nullable CharSequence contentDescription) {
-        return new LiquidTabItem(icon, null, contentDescription);
+        return new LiquidTabItem(icon, null, contentDescription, null);
     }
 
     @NonNull
     public static LiquidTabItem of(@NonNull Drawable icon, @NonNull Drawable selectedIcon,
                                    @Nullable CharSequence contentDescription) {
-        return new LiquidTabItem(icon, selectedIcon, contentDescription);
+        return new LiquidTabItem(icon, selectedIcon, contentDescription, null);
     }
 
     @NonNull
-    public Drawable getIcon() {
-        return icon;
+    public static LiquidTabItem of(@NonNull Drawable icon, @Nullable CharSequence contentDescription,
+                                   @Nullable CharSequence label) {
+        return new LiquidTabItem(icon, null, contentDescription, label);
     }
 
-    @Nullable
-    public Drawable getSelectedIcon() {
-        return selectedIcon;
+    @NonNull
+    public static LiquidTabItem of(@NonNull Drawable icon, @NonNull Drawable selectedIcon,
+                                   @Nullable CharSequence contentDescription, @Nullable CharSequence label) {
+        return new LiquidTabItem(icon, selectedIcon, contentDescription, label);
     }
 
+    @NonNull
+    public Drawable getIcon() { return icon; }
+
     @Nullable
-    public CharSequence getContentDescription() {
-        return contentDescription;
-    }
+    public Drawable getSelectedIcon() { return selectedIcon; }
+
+    @Nullable
+    public CharSequence getContentDescription() { return contentDescription; }
+    @Nullable
+    public CharSequence getLabel() { return label; }
 }
